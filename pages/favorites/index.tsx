af125903b0b4b1c5c68cf98922c8a714cab17540
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2022-03-08 17:00:45 -0600

    Pokemon Static

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+import { Layout } from '../../components/layouts'
+import { FavoritePokemons } from '../../components/pokemon';
+import { NoFavorites } from '../../components/ui';
+import { localFavorites } from '../../utils';
+
+const FavoritesPage = () => {
+
+  const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+
+  useEffect(() => {
+    setFavoritePokemons(localFavorites.pokemons());
+  }, []);
+
+  return (
+    <Layout>
+      {
+        favoritePokemons.length === 0 
+          ? (<NoFavorites />)
+          : (
+            <FavoritePokemons pokemons={favoritePokemons} />
+          )
+      }
+    </Layout>
+  )
+}
+
+export default FavoritesPage;
