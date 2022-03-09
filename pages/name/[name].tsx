a5dbb23ce22ce3e1f49ed487df0a2783698b1c45
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2022-03-09 11:39:26 -0600

    add ISG

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -112,7 +112,8 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemonNames.map( name => ({
       params: { name }
     })),
-    fallback: false
+    // fallback: false
+    fallback: 'blocking'
   }
 }
 
@@ -120,10 +121,22 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
 
   const { name } = params as { name: string };
 
+  const pokemon = await getPokemonInfo(name);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(name)
-    }
+      pokemon
+    },
+    revalidate: 86400, // 60 * 60 * 24
   }
 }
 
